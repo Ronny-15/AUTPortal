bcd085373f2318de9fb102645591478013ac7152
Ronny Patel <qvz3326@autuni.ac.nz>
2025-06-10 21:21:35 +1200

    Created classes for GUI Menus and did simple code for JFrame. Add Derby files for database.

---
+++ src/AUTPortal/MainFrame.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AUTPortal;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ronak
+ */
+public class MainFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("AUTPortal");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LoginPanel panel = new LoginPanel();
+        frame.add(panel);
+        frame.setSize(600,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+    }
+}
---
+++ src/AUTPortal/StudentMenuPanel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AUTPortal;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ronak
+ */
+public class StudentMenuPanel extends JPanel {
+    
+}
